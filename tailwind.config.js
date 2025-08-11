fb2225e05cdddfd87b73727397f8feb4cbec9a7b
jawad <jawadahmad9938@gmail.com>
2025-08-11 12:39:58 +0500

    new updates

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-black-fade': 'linear-gradient(to bottom, #1a202c, #000000)',
+        'navbar-fade': 'linear-gradient(to right, #2d3748, #000000)',
+        'dark-elegant': 'linear-gradient(to bottom right, #4a5568, #2d3748, #000000)',
+      },
+    },
+  },
+  plugins: [],
+}
